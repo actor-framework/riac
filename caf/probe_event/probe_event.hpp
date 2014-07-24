b1dd610af29f169f6348b691b9cc643194b62eed
Alex Mantel <alexmantel93@gmail.com>
2014-07-24 17:04:20 +0000

    Add struct net_info
    net_info contains mac, ipv4 and a vector over ipv6 addresses. add to
    node_info hostname, os and vector over net_info.

--- caf/probe_event/probe_event.hpp
+++ caf/probe_event/probe_event.hpp
@@ -6,44 +6,54 @@
 namespace caf {
 namespace probe_event {
 
+
 // send periodically from ActorProbe to ActorNexus
 struct ram_usage {
   uint64_t in_use;
   uint64_t available;
 };
- 
+
 // send periodically from ActorProbe to ActorNexus
 struct work_load {
-  uint8_t cpu_load;      // in percent, i.e., 0-100
-  uint64_t num_processes;
-  uint64_t num_actors;
+  uint8_t   cpu_load;      // in percent, i.e., 0-100
+  uint64_t  num_processes;
+  uint64_t  num_actors;
 };
- 
+
 struct cpu_info {
-  uint64_t num_cores;
-  uint64_t mhz_per_core;
+  uint64_t  num_cores;
+  uint64_t  mhz_per_core;
 };
- 
+
+struct net_info {
+  std::string               mac;
+  std::string               ipv4;
+  std::vector<std::string>  ipv6;
+};
+
 // send on connect from ActorProbe to ActorNexus
 struct node_info {
-  node_id id;
-  std::vector<cpu_info> cpu;
+  node_id                   id;
+  std::vector<cpu_info>     cpu;
+  std::string               hostname;
+  std::string               os;
+  std::vector<net_info>     net_infos;
 };
- 
-// send from ActorProbe to ActorNexus whenever from learns a new direct or 
+
+// send from ActorProbe to ActorNexus whenever from learns a new direct or
 // indirect route to another node
 struct new_route {
-  node_id from;
-  node_id to;
-  bool is_direct;
+  node_id   from;
+  node_id   to;
+  bool      is_direct;
 };
- 
+
 struct new_message {
-  node_id source_node;
-  node_id dest_node;
-  actor_id source_actor;
-  actor_id dest_actor;
-  message msg;
+  node_id   source_node;
+  node_id   dest_node;
+  actor_id  source_actor;
+  actor_id  dest_actor;
+  message   msg;
 };
 
 inline void announce_all() {
