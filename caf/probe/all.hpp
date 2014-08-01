d3b082db09ff97e0df8c068422b2229b9cae3a1b
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2014-08-01 16:04:08 +0200

    Initial commit

---
+++ caf/probe/all.hpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+ *                       ____    _    _____                                   *
+ *                      / ___|  / \  |  ___|    C++                           *
+ *                     | |     / _ \ | |_       Actor                         *
+ *                     | |___ / ___ \|  _|      Framework                     *
+ *                      \____/_/   \_|_|                                      *
+ *                                                                            *
+ * Copyright (C) 2011 - 2014                                                  *
+ * Dominik Charousset <dominik.charousset (at) haw-hamburg.de>                *
+ *                                                                            *
+ * Distributed under the terms and conditions of the BSD 3-Clause License or  *
+ * (at your option) under the terms and conditions of the Boost Software      *
+ * License 1.0. See accompanying files LICENSE and LICENCE_ALTERNATIVE.       *
+ *                                                                            *
+ * If you did not receive a copy of the license files, see                    *
+ * http://opensource.org/licenses/BSD-3-Clause and                            *
+ * http://www.boost.org/LICENSE_1_0.txt.                                      *
+ ******************************************************************************/
+
+#ifndef CAF_PROBE_ALL_HPP
+#define CAF_PROBE_ALL_HPP
+
+#include "caf/probe/init.hpp"
+
+#endif // CAF_PROBE_ALL_HPP
+
